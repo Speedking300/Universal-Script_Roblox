1e6c3595292097c7110575f5f470d9c254f65b3d
Speedking <166304031+Speedking300@users.noreply.github.com>
2025-01-20 19:02:17 +0100

    Update Universal-Script-.lua

--- Universal-Script-.lua
+++ Universal-Script-.lua
@@ -57,23 +57,6 @@ local function FlyLogic()
     end)
 end
 
--- Toggle for flying
-local Toggle = MainTab:CreateToggle({
-    Name = "Fly Toggle",
-    CurrentValue = false,
-    Flag = "FlyToggle1",
-    Callback = function(Value)
-        flying = Value
-        if flying then
-            FlyLogic()
-        else
-            local player = game.Players.LocalPlayer
-            local character = player.Character or player.CharacterAdded:Wait()
-            local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
-            humanoidRootPart.Velocity = Vector3.zero -- Stop movement
-        end
-    end,
-})
 
 ------------------------------------------------------------
 
